115dd9b660e41fc668904187908b74f515321108
elijahthomas1 <thomas.elijah@sympatico.ca>
2023-04-04 19:20:49 -0400

    updated css

---
+++ src/components/LocationBanner/LocationBanner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LocationBanner = () => {
+  return (
+    <div>LocationBanner</div>
+  )
+}
+
+export default LocationBanner
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import { FaSearch ,FaMicrophone } from 'react-icons/fa';
+import { BsCart3 } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <nav className='nav__nav navbar navbar-expand-lg'>
+    <nav className='nav__nav'>
       <div className='nav__div1'>
         <a className='nav__logo' href='#home'>Phone Store</a>
-        <div className='collapse navbar-collapse nav__div'>
-          <a className='nav-link nav__link' href="#home">Shop</a>
-          <a className='nav-link nav__link' href="#home">Support</a>
-          <a className='nav-link nav__link' href="#home">Account</a>
+        <div className='nav__div'>
+          <a className='nav__link' href="#home">Shop</a>
+          <a className='nav__link' href="#home">Support</a>
+          <a className='nav__link' href="#home">Account</a>
         </div>
       </div>
-      <div>
-        
+      <div className='nav__div2'>
+        <form className='nav__form'>
+          <input className='nav__form-input' type="text" placeholder='Search this site' />
+          <button className='nav__form-search'><FaSearch /></button>
+          <button className='nav__form-microphone'><FaMicrophone /></button>
+        </form>
+        <button className='nav__login'>Log In / Register</button>
+        <BsCart3 className='nav__cart' />
       </div>
     </nav>
   )
