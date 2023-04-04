0a3f6fffe4c07cbeded916cdcc7464d37e264702
elijahthomas1 <thomas.elijah@sympatico.ca>
2023-04-04 18:28:08 -0400

    updated navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav__nav navbar navbar-expand-lg'>
+      <div className='nav__div1'>
+        <a className='nav__logo' href='#home'>Phone Store</a>
+        <div className='collapse navbar-collapse nav__div'>
+          <a className='nav-link nav__link' href="#home">Shop</a>
+          <a className='nav-link nav__link' href="#home">Support</a>
+          <a className='nav-link nav__link' href="#home">Account</a>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </nav>
   )
 }
 
