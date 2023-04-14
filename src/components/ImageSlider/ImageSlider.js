78a366b2ea1da290fbfce2a79a575bab265d023e
elijahthomas1 <thomas.elijah@sympatico.ca>
2023-04-13 22:56:05 -0400

    edited imageSlider

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -9,8 +9,10 @@ const ImageSlider = ({images}) => {
       {images.map((image,index) => {
         return (<div className={index === 0 ? 'slider__div img_active' : 'slider__div img_inactive'}>
                   <img className='slider__img' src={image.image} alt={image.title} />
-                  <div>
-                    
+                  <div className='slider__text'>
+                    <h3 className='slider__text-title'>{image.title}</h3>
+                    <p className='slider__text-description'>{image.description}</p>
+                    <button className='slider__text-button'>{image.buttonText}</button>
                   </div>
                 </div>
         )
