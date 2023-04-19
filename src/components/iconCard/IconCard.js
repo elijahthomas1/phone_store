8e2226dc5207f5f864f5bcc98874396a0e736322
elijahthomas1 <thomas.elijah@sympatico.ca>
2023-04-18 23:42:30 -0400

    added icon bar

---
+++ src/components/iconCard/IconCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IconCard = ({image}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IconCard
---
+++ src/components/iconbar/Iconbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Iconbar = () => {
+  return (
+    <div>Iconbar</div>
+  )
+}
+
+export default Iconbar
